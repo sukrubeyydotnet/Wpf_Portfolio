d7a8154bb4dcf7e5caaf1769ce41cdcc749af228
Şükrü <sukrubeyy7@gmail.com>
2024-03-12 01:10:10 +0300

    Home,Main,Education,Projects ViewModal - INotifyPropertyChanged - RelayCommand - Home, Main, Education,Projects View created

---
+++ UIFlatDesign_WPF/MVMM/ViewModal/MainViewModal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UIFlatDesign_WPF.Core;
+
+namespace UIFlatDesign_WPF.MVMM.ViewModal
+{
+    class MainViewModal : ObservableObject
+    {
+        private object _currentView;
+
+        public HomeViewModal homeViemModal { get; set; }
+        public ProjectsViewModal projectsViewModal { get; set; }
+        public EducationViewModal educationViewModal { get; set; }
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand ProjectsViewCommand { get; set; }
+        public RelayCommand EducationViewCommand { get; set; }
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModal()
+        {
+            homeViemModal = new HomeViewModal();
+            projectsViewModal = new ProjectsViewModal();
+            educationViewModal = new EducationViewModal();
+
+            _currentView = homeViemModal;
+
+
+            HomeViewCommand = new RelayCommand(x =>
+            {
+                CurrentView = homeViemModal;
+            });
+            ProjectsViewCommand = new RelayCommand(x =>
+            {
+                CurrentView = projectsViewModal;
+            });
+            EducationViewCommand = new RelayCommand(x =>
+            {
+                CurrentView = educationViewModal;
+            });
+
+        }
+    }
+}
---
+++ UIFlatDesign_WPF/MVMM/ViewModal/ProjectsViewModal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIFlatDesign_WPF.Core;
+namespace UIFlatDesign_WPF.MVMM.ViewModal
+{
+    internal class ProjectsViewModal 
+    {
+    }
+}
